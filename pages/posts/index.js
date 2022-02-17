07f974eced115fc6f13828a1b47e72aca90d17b1
Svetoslav99 <52759406+Svetoslav99@users.noreply.github.com>
2022-02-17 19:49:10 +0200

    Initial commit

---
+++ pages/posts/index.js
@@ -0,0 +1,31 @@
+import Head from 'next/head';
+import { Fragment } from 'react';
+
+import AllPosts from '../../components/posts/all-posts';
+import { getAllPosts } from '../../lib/posts-util';
+
+function AllPostsPage(props) {
+    return (
+        <Fragment>
+            <Head>
+                <title>All Posts</title>
+                <meta
+                    name='description'
+                    content='A list of all programming-related tutorials and posts!'
+                />
+            </Head>
+            <AllPosts posts={props.allPosts} />
+        </Fragment>
+    );
+}
+
+export function getStaticProps() {
+    const allPosts = getAllPosts();
+    return {
+        props: {
+            allPosts: allPosts,
+        },
+    };
+}
+
+export default AllPostsPage;
